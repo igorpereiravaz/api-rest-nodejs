cd73b875153672eae6f76e48ff7b929ad6b1d34e
igorvaz-pravaler <igor.vaz@pravaler.com.br>
2023-07-26 14:37:07 -0300

    build(api-rest):

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,26 @@
 import { table } from 'console'
+import { randomUUID } from 'crypto'
 import fastify from 'fastify'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  // const tables = await knex('sqlite_schema').select('*')
 
-  return tables
+  // const transaction = await knex('transactions')
+  //   .insert({
+  //     id: randomUUID(),
+  //     title: 'Transação de Teste',
+  //     amount: 1000,
+  //   })
+  //   .returning('*')
+
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transactions
 })
 
 app
