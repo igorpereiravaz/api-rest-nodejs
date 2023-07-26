f9172c0fbd5f312e68b4627dc305017765b7f355
igorvaz-pravaler <igor.vaz@pravaler.com.br>
2023-07-26 11:53:01 -0300

    feat(api-rest):

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
+import { table } from 'console'
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello Word'
+app.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return table
 })
 
 app
