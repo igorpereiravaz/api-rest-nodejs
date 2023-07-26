e45376123204c81417df62c173a08a3efa626cd3
igorvaz-pravaler <igor.vaz@pravaler.com.br>
2023-07-26 11:10:32 -0300

    build(api-rest): :rocket: crete application

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/hello', () =>{
+    return 'Hello Word'
+})
+
+app.listen({
+    port: 3333,
+}).then(() =>{
+    console.log('HTTP Server Running!')
+})
